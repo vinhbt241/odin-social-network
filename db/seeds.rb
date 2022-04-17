8f369074e53d21eeb34d4e07abe66ae7bbe8f9ec
Vinh <vinhbuith32412000@gmail.com>
2022-04-17 09:07:32 +0700

    Seed users for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user1 = User.new
+user1.name = "Vinh"
+user1.email = 'vinh@gmail.com'
+user1.password = "Vinh241"
+user1.password_confirmation = "Vinh241"
+user1.save!
+
+user2 = User.new
+user2.name = "Viktor"
+user2.email = 'viktor@gmail.com'
+user2.password = "Viktor241"
+user2.password_confirmation = "Viktor241"
+user2.save!
+
+user3 = User.new
+user3.name = "Viola"
+user3.email = "viola@gmail.com"
+user3.password = "Viola241"
+user3.password_confirmation = "Viola241"
+user3.save!
