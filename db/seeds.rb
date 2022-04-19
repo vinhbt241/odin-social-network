80e5e12a6c5059b9318c365123ce77c7a29ec59b
Vinh <vinhbuith32412000@gmail.com>
2022-04-19 09:01:42 +0700

    Test if post and comment model working properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,8 @@ user3.email = "viola@gmail.com"
 user3.password = "Viola241"
 user3.password_confirmation = "Viola241"
 user3.save!
+
+Post.create(topic: "Post of Vinh", content: "This is a test post to see if it's working properly", author_id: 1)
+Post.create(topic: "Post of Viktor", content: "This is second test post, gonna check if comments working properly", author_id: 2)
+
+Comment.create(user_id: 1, post_id: 2, content: "This is Vinh's comment")
