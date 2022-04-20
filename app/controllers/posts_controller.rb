11afcaa1d2c2e89c7019905be00f30a3f9009155
Vinh <vinhbuith32412000@gmail.com>
2022-04-20 21:26:08 +0700

    Check if devise working properly and set link to logout + display flash message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index 
-    @posts = current_user.post.all + current_user.friends.map(&:posts)
+    @posts = current_user.posts.all + current_user.friends.map(&:posts)
     @posts = @posts.flatten
-    @posts = @posts.sort_by { |posts| post.created_at }
+    @posts = @posts.sort_by { |post| post.created_at }
     @posts = @posts.reverse
   end
 end
