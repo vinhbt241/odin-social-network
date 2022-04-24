5bce26c7252f77ab00c2ab7f8f345e4cd10f1e5a
Vinh <vinhbuith32412000@gmail.com>
2022-04-24 15:38:07 +0700

    Make sure transition from friend request to friendship work right

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,6 +2,7 @@ class FriendshipsController < ApplicationController
   def create 
     current_user.friend_rights << User.find(params[:requester_id])
     FriendRequest.where(requester_id: params[:requester_id]).where(addressee_id: current_user.id).delete_all
+    FriendRequest.where(requester_id: current_user.id).where(addressee_id: params[:requester_id]).delete_all
 
     flash.notice = "Friend Request Accepted!"
     redirect_to friend_requests_path, status: 303
