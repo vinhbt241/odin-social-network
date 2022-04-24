797ac923227d3582b2645231c65b3e75448c0330
Vinh <vinhbuith32412000@gmail.com>
2022-04-24 15:22:33 +0700

    Add function to allow user create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,21 @@ class PostsController < ApplicationController
     @posts = @posts.sort_by { |post| post.created_at }
     @posts = @posts.reverse
   end
+
+  def create 
+    @post = Post.new(post_params)
+
+    if @post.save 
+      flash.notice = "Post created successfully"
+      redirect_to posts_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def post_params 
+    params.permit(:topic, :content, :author_id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
 
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates :topic, presence: true
+  validates :content, presence: true 
+  validates :author_id, presence: true
 end
